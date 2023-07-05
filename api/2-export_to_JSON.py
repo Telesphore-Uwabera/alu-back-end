b032c5f73e2ad2e8eed5a65173a55d8761539be1
TelesphoreUwabera <t.uwabera@alustudent.com>
2023-07-05 14:16:50 +0200

    Update 2-export_to_JSON.py

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -14,20 +14,20 @@ def get_employee_todo_list(employee_id):
     Retrieves and exports the TODO list progress for a given employee ID in JSON format.
     """
 
-    # Make a GET request to the API endpoint
+    # Making a GET request to the API endpoint
     response = requests.get(
         'https://jsonplaceholder.typicode.com/todos',
         params={'userId': employee_id}
     )
 
-    # Check if the request was successful
+    # Checking if the request was successful
     if response.status_code != 200:
         print(f"Error: Failed to retrieve TODO list for employee {employee_id}")
         return
 
     todos = response.json()
 
-    # Prepare the JSON filename
+    # Preparing the JSON filename
     filename = f"{employee_id}.json"
 
     # Create a dictionary to store the tasks
@@ -42,7 +42,7 @@ def get_employee_todo_list(employee_id):
         }
         tasks_dict["USER_ID"].append(task_info)
 
-    # Write the dictionary to a JSON file
+    # Writing the dictionary to a JSON file
     with open(filename, 'w') as file:
         json.dump(tasks_dict, file)
 
@@ -52,4 +52,3 @@ def get_employee_todo_list(employee_id):
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage:
-
