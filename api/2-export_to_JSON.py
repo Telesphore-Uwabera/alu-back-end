d53ab0125953cbdb451e4bec3461a4fa374abb3f
TelesphoreUwabera <t.uwabera@alustudent.com>
2023-07-09 22:57:06 +0200

    Update 2-export_to_JSON.py

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,46 +1,27 @@
 #!/usr/bin/python3
-""" Call API and store data in JSON """
-import json
-import urllib.parse
-import urllib.request
-from sys import argv
+""""Module"""
 
+import json
+import requests
+import sys
 
 if __name__ == '__main__':
-    employee_id = argv[1]
-    url_todo = 'https://jsonplaceholder.typicode.com/todos/'
-    url_user = 'https://jsonplaceholder.typicode.com/users/'
+    employee_id = sys.argv[1]
+    user_url = "https://jsonplaceholder.typicode.com/users/{}" \
+        .format(employee_id)
+    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos/" \
+        .format(employee_id)
 
-    user_params = {'id': employee_id}
-    user_url_with_params = url_user + '?' + urllib.parse.urlencode(user_params)
-    with urllib.request.urlopen(user_url_with_params) as user_response:
-        user_dict_list = json.loads(user_response.read().decode())
+    user_info = requests.request('GET', user_url).json()
+    todos_info = requests.request('GET', todos_url).json()
 
-    if not user_dict_list:
-        print("Invalid USER_ID")
-        exit()
+    employee_username = user_info["username"]
 
-    employee_username = user_dict_list[0].get('username')
+    todos_info_sorted = [
+        dict(zip(["task", "completed", "username"],
+                 [task["title"], task["completed"], employee_username]))
+        for task in todos_info]
 
-    todo_params = {'userId': employee_id}
-    todo_url_with_params = url_todo + '?' + urllib.parse.urlencode(todo_params)
-    with urllib.request.urlopen(todo_url_with_params) as todo_response:
-        todo_dict_list = json.loads(todo_response.read().decode())
-
-    tasks = []
-    for task in todo_dict_list:
-        task_title = task['title']
-        task_completed = task['completed']
-        task_data = {
-            'task': task_title,
-            'completed': task_completed,
-            'username': employee_username
-        }
-        tasks.append(task_data)
-
-    data = {employee_id: tasks}
-
-    with open(f'{employee_id}.json', 'w') as json_file:
-        json.dump(data, json_file)
-
-    print(f"Correct {employee_id}: OK")
+    user_dict = {str(employee_id): todos_info_sorted}
+    with open(str(employee_id) + '.json', "w") as file:
+        file.write(json.dumps(user_dict))
