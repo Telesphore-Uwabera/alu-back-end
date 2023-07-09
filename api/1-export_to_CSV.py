52e6c93e9dd6b58d861ab8c2a93e12dbf7b1c6ec
TelesphoreUwabera <t.uwabera@alustudent.com>
2023-07-09 22:20:45 +0200

    Update 1-export_to_CSV.py

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,38 +1,22 @@
 #!/usr/bin/python3
-""" Call API and store data in CSV """
+"""get TODO list"""
+
 import csv
 import json
-import urllib.parse
-import urllib.request
-from sys import argv
-
-
-if __name__ == '__main__':
-    employee_id = argv[1]
-    url_todo = 'https://jsonplaceholder.typicode.com/todos/'
-    url_user = 'https://jsonplaceholder.typicode.com/users/'
-
-    todo_params = {'userId': employee_id}
-    todo_url_with_params = url_todo + '?' + \
-        urllib.parse.urlencode(todo_params)
-    with urllib.request.urlopen(todo_url_with_params) as todo_response:
-        todo_dict_list = json.loads(todo_response.read().decode())
-
-    user_params = {'id': employee_id}
-    user_url_with_params = url_user + '?' + \
-        urllib.parse.urlencode(user_params)
-    with urllib.request.urlopen(user_url_with_params) as user_response:
-        user_dict_list = json.loads(user_response.read().decode())
-
-    employee = user_dict_list[0].get('username')
-
-    with open(f'{employee_id}.csv', 'w', newline='') as csvfile:
-        csvwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        csvwriter.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS",
-                            "TASK_TITLE"])
-        for task in todo_dict_list:
-            status = task['completed']
-            title = task['title']
-            csvwriter.writerow([employee_id, employee, status, title])
-
-    print(f"File {employee_id}.csv has been created with the task data.")
+import requests
+import sys
+if __name__ == "__main__":
+    link = "https://jsonplaceholder.typicode.com/users/{}".format(sys.argv[1])
+    res = requests.get(link)
+    user = json.loads(res.text)
+    num = sys.argv[1]
+    link = "https://jsonplaceholder.typicode.com/users/{}/todos".format(num)
+    res = requests.get(link)
+    todos = json.loads(res.text)
+    csv_data = [["{}".format(i["userId"]),
+                user["username"],
+                "{}".format(i["completed"]),
+                 i["title"]] for i in todos]
+    with open("{}.csv".format(user["id"]), 'w', encoding='utf-8') as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
+        writer.writerows(csv_data)
